c73d20779dc2db0bc691a01545d6edf99ce41998
Epicpkmn11 <epicpkmn11@outlook.com>
2018-08-06 15:05:53 -0500

    Redid most of the code to use lists instead of massive amounts of 'if's

--- Check_DSi.py
+++ Check_DSi.py
@@ -1,147 +1,58 @@
 import os
 import sys
+
 #checks for python 3
 if(sys.version_info<(3,0,0)):
 	print('You are not using Python 3, please use Python 3 to run this script')
 	input('Press Enter to close')
 	quit()
 
-#Choosing what to check for
-region = input('What region is your DSi? (U/J/E/A) ').upper()
+#creating functions
+def end():
+	input('Press Enter to quit')
+	quit()
+
+#defining variables
+missingFiles = 'You\'re missing '
+files = ['']
+
+#choosing what to check for
 hiyacfw = input('Would you like to check for HiyaCFW files? (Y/N) ').upper()
-dsimenuplusplus = input('Would you like to check for DSiMenu++ files? (Y/N) ').upper()
-output = 'You are missing: '
-
-#Quit if both no
-if(hiyacfw != 'Y'):
-	if(dsimenuplusplus != 'Y'):
-		print('You have not selected to check for HiyaCFW or DSiMenu++ files')
-		input('Press Enter to close')
-		quit()
-
-#HiyaCFW checking
 if(hiyacfw == 'Y'):
-	if os.path.exists('./bootcode.dsi'):
-	    pass
-	else:
-	    output = output+'/bootcode.dsi, '
-	if os.path.exists('./hiya/bootloader.nds'):
-	    pass
-	else:
-	    output = output+'/hiya/bootloader.nds, '
-	if os.path.exists('./shared1/TWLCFG0.dat'):
-	    pass
-	else:
-	    output = output+'/shared1/TWLCFG0.dat, '
-	if os.path.exists('./shared1/TWLCFG1.dat'):
-	    pass
-	else:
-	    output = output+'/shared1/TWLCFG1.dat, '
-	if os.path.exists('./shared2/0000'):
-	    pass
-	else:
-	    output = output+'/shared2/0000, '
-	if os.path.exists('./shared2/launcher/wrap.bin'):
-	    pass
-	else:
-	    output = output+'/shared2/launcher/wrap.bin, '
-	if os.path.exists('./sys/cert.sys'):
-	    pass
-	else:
-	    output = output+'/sys/cert.sys, '
-	if os.path.exists('./sys/dev.kp'):
-	    pass
-	else:
-	    output = output+'/sys/dev.kp, '
-	if os.path.exists('./sys/HWID.sgn'):
-	    pass
-	else:
-	    output = output+'/sys/HWID.sgn, '
-	if os.path.exists('./sys/HWINFO_N.dat'):
-	    pass
-	else:
-	    output = output+'/sys/HWINFO_N.dat, '
-	if os.path.exists('./sys/HWINFO_S.dat'):
-	    pass
-	else:
-	    output = output+'/sys/HWINFO_S.dat, '
-	if os.path.exists('./sys/TWLFontTable.dat'):
-	    pass
-	else:
-	    output = output+'/sys/TWLFontTable.dat, '
+	region = input('What region is your DSi? (U/J/E/A) ').upper()
+dsimenuplusplus = input('Would you like to check for DSiMenu++ files? (Y/N) ').upper()
 
-	#Regional checking
+#determining files to check
+if(hiyacfw == 'Y'):
+	files = ['bootcode.dsi', 'hiya/bootloader.nds', 'shared1/TWLCFG0.dat', 'shared1/TWLCFG1.dat', 'shared2/0000', 'shared2/launcher/wrap.bin', 'sys/cert.sys', 'sys/dev.kp', 'sys/HWID.sgn', 'sys/HWINFO_N.dat', 'sys/HWINFO_S.dat', 'sys/TWLFontTable.dat']
 	if(region == 'U'):
-	    if os.path.exists('./title/00030017/484e4145/content/00000002.app'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e4145/content/00000002.app, '
-	    if os.path.exists('./title/00030017/484e4145/content/title.tmd'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e4145/content/title.tmd, '
+		files = files + ['title/00030017/484e4145/content/00000002.app', 'title/00030017/484e4145/content/title.tmd']
 	elif(region == 'J'):
-	    if os.path.exists('./title/00030017/484e414a/content/00000002.app'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e414a/content/00000002.app, '
-	    if os.path.exists('./title/00030017/484e414a/content/title.tmd'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e414a/content/title.tmd, '
+		files = files + ['title/00030017/484e414a/content/00000002.app', 'title/00030017/484e414a/content/title.tmd']
 	elif(region == 'E'):
-	    if os.path.exists('./title/00030017/484e4150/content/00000002.app'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e4150/content/00000002.app, '
-	    if os.path.exists('./title/00030017/484e4150/content/title.tmd'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e4150/content/title.tmd, '
+		files = files + ['title/00030017/484e4150/content/00000002.app', 'title/00030017/484e4150/content/title.tmd']
 	elif(region == 'A'):
-	    if os.path.exists('./title/00030017/484e4155/content/00000002.app'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e4155/content/00000002.app, '
-	    if os.path.exists('./title/00030017/484e4155/content/title.tmd'):
-	        pass
-	    else:
-	        output = output+'/title/00030017/484e4155/content/title.tmd, '
+		files = files + ['title/00030017/484e4155/content/00000002.app', 'title/00030017/484e4155/content/title.tmd']
 	else:
-	    print('Invalid region')
-	    input('Press Enter to quit')
-	    quit()
-
-#DSiMenu++ checking
+		print('Invalid Region')
+		end()
 if(dsimenuplusplus == 'Y'):
-	if os.path.exists('./BOOT.NDS'):
-		pass
-	else:
-		output = output+'/BOOT.NDS, '
-	if os.path.exists('./_nds'):
-		pass
-	else:
-		output = output+'/_nds, '
-	if os.path.exists('./title/00030015/534c524e/content/00000000.app'):
-		pass
-	else:
-		output = output+'/title/00030015/534c524e/content/00000000.app, '
-	if os.path.exists('./title/00030015/534c524e/content/title.tmd'):
-		pass
-	else:
-		output = output+'/title/00030015/534c524e/content/title.tmd, '
-	if os.path.exists('./title/00030015/53524c41/content/00000000.app'):
-		pass
-	else:
-		output = output+'/title/00030015/53524c41/content/00000000.app, '
-	if os.path.exists('./title/00030015/53524c41/content/00000000.app'):
-		pass
-	else:
-		output = output+'/title/00030015/53524c41/content/00000000.app, '
+	files = files + ['BOOT.NDS','_nds/GBARunner2_fc.nds', '_nds/GBARunner2.nds', '_nds/hb-bootstrap.nds', '_nds/nds-bootstrap.ini', '_nds/nightly-bootstrap-sdk5.nds', '_nds/nightly-bootstrap.nds', '_nds/release-bootstrap-sdk5.nds', '_nds/release-bootstrap.nds', '_nds/dsimenuplusplus/dsimenu.srldr', '_nds/dsimenuplusplus/gbaswitch.srldr', '_nds/dsimenuplusplus/main.srldr', '_nds/dsimenuplusplus/nightly-bootstrap', '_nds/dsimenuplusplus/nightly-bootstrap-sdk5', '_nds/dsimenuplusplus/r4menu.srldr', '_nds/dsimenuplusplus/release-bootstrap','_nds/dsimenuplusplus/release-bootstrap-sdk5', '_nds/dsimenuplusplus/slot1launch.srldr', '_nds/dsimenuplusplus/emulators/gameyob.nds', '_nds/dsimenuplusplus/emulators/nesds.nds', '_nds/dsimenuplusplus/emulators/nestwl.nds', 'title/00030015/534c524e/content/00000000.app', 'title/00030015/534c524e/content/title.tmd', 'title/00030015/53524c41/content/00000000.app', 'title/00030015/53524c41/content/title.tmd']
+if(dsimenuplusplus == hiyacfw != 'Y'):
+	print('You have not selected to check for HiyaCFW or DSiMenu++ files')
+	end()
 
-#Displaying results
-if(output != 'You are missing: '):
-    print(output)
+#checking files
+for file in files:
+	fileExists = os.path.exists(file)
+	if fileExists == False:
+		missingFiles = missingFiles + '/' + file + ', '
+
+#displaying results
+if(missingFiles != 'You\'re missing '):
+	missingFiles = missingFiles[:-2]
+	print(missingFiles)
+	end()
 else:
-    print('Your SD card is good!')
-input('Press Enter to quit')
+	print('Your SD Card is good!')
+	end()
