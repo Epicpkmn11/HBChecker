4ebc6929d7b261a3e9ef9cd6b101f1e985cb0bd1
Epicpkmn11 <epicpkmn11@outlook.com>
2018-08-31 00:55:48 -0500

    Rewrite a bunch of code, add force checking, add Python 2 compatibility, add descriptions of where files come from
    Force Checking at line 111
    Python 2 compatibility at line 5
    Checks for '\n' and removes it, 1 before, and all after when checking, but shows it when printing results at the end

--- Check_DSi.py
+++ Check_DSi.py
@@ -2,12 +2,11 @@ import os
 import sys
 import glob
 
-#checks for python 3
-if(sys.version_info<(3,0,0)):
-	print('You are not using Python 3, please use Python 3 to run this script')
-	input('Press Enter to close')
-	quit()
+# Fixes Python 2 compatibility
+if(sys.version_info.major < (3)):
+	input = raw_input
 
+# Creating variables
 filesMissing = []
 scannedFiles = []
 sdFiles = []
@@ -17,78 +16,85 @@ isDSiMemuPPPresnt = False
 # Lists of required files
 requiredFiles = {
 	'hiyaCFW': [
-		'bootcode.dsi', 
-		'hiya/bootloader.nds', 
-		'shared1/TWLCFG0.dat', 
-		'shared1/TWLCFG1.dat', 
-		'shared2/0000', 
-		'shared2/launcher/wrap.bin', 
-		'sys/cert.sys', 
-		'sys/dev.kp',
-		'sys/HWID.sgn', 
-		'sys/HWINFO_N.dat', 
-		'sys/HWINFO_S.dat', 
-		'sys/TWLFontTable.dat'
+		'bootcode.dsi \nFrom the downloaded "HiyaCFW/for SDNAND SD card"',
+		'hiya/bootloader.nds \nFrom the downloaded "HiyaCFW/for PC/Modified Files" after running HiyaCFW Helper',
+		'shared1/TWLCFG0.dat \nFrom your NAND backup',
+		'shared1/TWLCFG1.dat \nFrom your NAND backup',
+		'shared2/0000 \nFrom your NAND backup',
+		'shared2/launcher/wrap.bin \nFrom your NAND backup',
+		'sys/cert.sys \nFrom your NAND backup',
+		'sys/dev.kp \nFrom your NAND backup',
+		'sys/HWID.sgn \nFrom your NAND backup',
+		'sys/HWINFO_N.dat \nFrom your NAND backup',
+		'sys/HWINFO_S.dat \nFrom your NAND backup',
+		'sys/TWLFontTable.dat \nFrom your NAND backup'
 	],
 	'dsimenuplusplus': [
-		'BOOT.NDS', 
-		'_nds/GBARunner2_fc.nds', 
-		'_nds/GBARunner2.nds', 
-		'_nds/nds-bootstrap-hb-nightly.nds', 
-		'_nds/nds-bootstrap-hb-release.nds', 
-		'_nds/nds-bootstrap-nightly.nds', 
-		'_nds/nds-bootstrap-release.nds', 
-		'_nds/nds-bootstrap.ini', 
-		'_nds/dsimenuplusplus/dsimenu.srldr', 
-		'_nds/dsimenuplusplus/gbaswitch.srldr', 
-		'_nds/dsimenuplusplus/main.srldr', 
-		'_nds/dsimenuplusplus/nightly-bootstrap', 
-		'_nds/dsimenuplusplus/r4menu.srldr', 
-		'_nds/dsimenuplusplus/release-bootstrap', 
-		'_nds/dsimenuplusplus/slot1launch.srldr', 
-		'_nds/dsimenuplusplus/emulators/gameyob.nds', 
-		'_nds/dsimenuplusplus/emulators/nesds.nds', 
-		'_nds/dsimenuplusplus/emulators/nestwl.nds', 
-		'hiya/autoboot.bin', 
-		'title/00030015/534c524e/content/00000000.app', 
-		'title/00030015/534c524e/content/title.tmd', 
-		'title/00030015/53524c41/content/00000000.app', 
-		'title/00030015/53524c41/content/title.tmd'
-	]			
+		'BOOT.NDS \nFrom the downloaded "DSiMenuPP"',
+		'_nds/GBARunner2_fc.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/GBARunner2.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/nds-bootstrap-hb-nightly.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/nds-bootstrap-hb-release.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/nds-bootstrap-nightly.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/nds-bootstrap-release.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/nds-bootstrap.ini \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/dsimenu.srldr \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/gbaswitch.srldr \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/main.srldr \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/nightly-bootstrap \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/r4menu.srldr \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/release-bootstrap \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/slot1launch.srldr \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/emulators/gameyob.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/emulators/nesds.nds \nFrom the downloaded "DSiMenuPP"',
+		'_nds/dsimenuplusplus/emulators/nestwl.nds \nFrom the downloaded "DSiMenuPP"',
+		'hiya/autoboot.bin \nFrom the downloaded "DSiMenuPP/Autoboot for HiyaCFW"',
+		'title/00030015/534c524e/content/00000000.app \nFrom the downloaded "DSiMenuPP/CFW - SDNAND root"',
+		'title/00030015/534c524e/content/title.tmd \nFrom the downloaded "DSiMenuPP/CFW - SDNAND root"',
+		'title/00030015/53524c41/content/00000000.app \nFrom the downloaded "DSiMenuPP/CFW - SDNAND root"',
+		'title/00030015/53524c41/content/title.tmd \nFrom the downloaded "DSiMenuPP/CFW - SDNAND root"'
+	]
 }
 if __name__ == "__main__":
-	# Scan the files in the current work directory.
+	# Scan the files in the current working directory.
 	for path, subdirs, files in os.walk('.'):
 		for name in files:
 			filePath = os.path.join(os.getcwd(), path[2:], name)
 			scannedFiles.append(filePath)
 
-	#checking files
+	# Checks files
 	for file in scannedFiles:
-		# checks if your on Windows and removes the file path before the cwd
+		# Checks if your on Windows and removes the file path before the cwd
 		if sys.platform == "win32":
 			currentFile = file[len(os.getcwd()):]
 		else:
 			currentFile = file[(len(os.getcwd())+1):]
 		# Check if HiyaCFW is installed.
-		# print(currentFile)
-		if currentFile == os.path.join("hiya","settings.ini"):
+		if currentFile == ("bootcode.dsi"):
 			isHiyaPresent = True
 			print("HiyaCFW files detected, verifying files...")
 			HiyaFiles = requiredFiles['hiyaCFW']
 			region = input('What region is your DSi? (U/J/E/A) ').upper()
 			while region:
 				if(region == 'U'):
-					HiyaFiles.extend(['title/00030017/484e4145/content/title.tmd', 'title/00030017/484e4145/content/title.tmd'])
+					HiyaFiles.extend(
+						['title/00030017/484e4145/content/00000002.app \nFrom the downloaded "HiyaCFW/for PC/Modified Files" after running HiyaCFW Helper',
+						'title/00030017/484e4145/content/title.tmd \nFrom the downloaded "HiyaCFW/for SDNAND SD card"'])
 					break
 				elif(region == 'J'):
-					HiyaFiles.extend(['title/00030017/484e414a/content/00000002.app', 'title/00030017/484e414a/content/title.tmd'])
+					HiyaFiles.extend(
+						['title/00030017/484e414a/content/00000002.app \nFrom the downloaded "HiyaCFW/for PC/Modified Files" after running HiyaCFW Helper',
+						'title/00030017/484e414a/content/title.tmd \nFrom the downloaded "HiyaCFW/for SDNAND SD card"'])
 					break
 				elif(region == 'E'):
-					HiyaFiles.extend(['title/00030017/484e4150/content/00000002.app', 'title/00030017/484e4150/content/title.tmd'])
+					HiyaFiles.extend(
+						['title/00030017/484e4150/content/00000002.app \nFrom the downloaded "HiyaCFW/for PC/Modified Files" after running HiyaCFW Helper',
+						'title/00030017/484e4150/content/title.tmd \nFrom the downloaded "HiyaCFW/for SDNAND SD card"'])
 					break
 				elif(region == 'A'):
-					HiyaFiles.extend(['title/00030017/484e4155/content/00000002.app', 'title/00030017/484e4155/content/title.tmd'])
+					HiyaFiles.extend(
+						['title/00030017/484e4155/content/00000002.app \nFrom the downloaded "HiyaCFW/for PC/Modified Files" after running HiyaCFW Helper',
+						'title/00030017/484e4155/content/title.tmd \nFrom the downloaded "HiyaCFW/for SDNAND SD card"'])
 					break
 				else:
 					print('Invalid Region')
@@ -97,19 +103,28 @@ if __name__ == "__main__":
 		# Check if DSiMenuPlusPlus is installed.
 		elif currentFile == os.path.join("_nds", "dsimenuplusplus", "main.srldr"):
 			isDSiMemuPPPresnt = True
-			print("DSiMenuPlusPlus files detected, verifying files...")
+			print("DSiMenu++ files detected, verifying files...")
 			sdFiles.extend(requiredFiles['dsimenuplusplus'])
 		else:
 			pass
 
+	# Checking if Hiya/DSiMenu++ are missing and lets you force a check
 	if isHiyaPresent == False:
 		print("HiyaCFW NOT detected")
+		if input("Force check HiyaCFW? (Doesn't check region specific files) (Y/N) ").upper() == 'Y':
+			sdFiles.extend(requiredFiles['hiyaCFW'])
 	if isDSiMemuPPPresnt == False:
 		print("DSiMenu++ NOT detected")
+		if input("Force check DSiMenu++? (Y/N) ").upper() == 'Y':
+			sdFiles.extend(requiredFiles['dsimenuplusplus'])		
 
 	fileTree = sdFiles
 	for file in fileTree:
-		filePath = os.path.join(os.getcwd(), file)
+		fileCopy = file
+		comment = fileCopy.find('\n')
+		if comment != -1:
+			fileCopy = fileCopy[:(comment-1)]
+		filePath = os.path.join(os.getcwd(), fileCopy)
 		if os.path.exists(filePath):
 			pass
 		else:
@@ -123,4 +138,3 @@ if __name__ == "__main__":
 
 	input('Press Enter to quit')
 	quit()
-
